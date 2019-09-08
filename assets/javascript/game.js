f1bde472afa13742b8856a134744a964e27987e6
lambertco <blambertco@gmail.com>
2019-09-08 12:00:23 -0600

    changed game theme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,11 +18,11 @@
 
 
 // Global variables
-var wins = 0;
-var numOfGuesses = 10;
-var guess = [];
-var currentWord = ["_"];
 var words = ["athens", "santorini", "mykonos", "crete", "zakynthos", "delphi", "delos"];
+var guess = [];
+var currentWord = [];
+var numOfGuesses = 10;
+var wins = 0;
 
 // variables for quick reference to html
 var directionsText = document.getElementById("directions");
@@ -31,87 +31,46 @@ var numOfGuessesText = document.getElementById("remaining");
 var winsText = document.getElementById("wins");
 var guessText = document.getElementById("guess");
 
-
 // player presses a key to get started
 document.onkeyup = function (event) {
-    var guess = event.key;
+    var userGuess = event.key;
     // the game picks a random word to start
     var word = words[Math.floor(Math.random() * words.length)];
-    console.log(word);
+    prompt("Guess a letter");
 }
 function checkForLetter(letter) {
     var foundLetter = false
-for (var i=0, j= words.length; i<j; i++) {
-    if (letter === words[i]) {
-        guess[i] = letter
-        foundLetter = true
-        // If guessing word matches random word
-        if (guess.join("") === words) {
-            // Increment # of wins
-            wins++
+    for (var i = 0, j = words.length; i < j; i++) {
+        if (letter === words[i]) {
+            guess[i] = letter
+            foundLetter = true
+            // If guess matches random word
+            if (guess.join("") === words) {
+                // Increment # of wins
+                wins++
+            }
+        }
+    }
+
+    if (!foundLetter) {
+        // Check if incorrect guess is already on the list
+        if (!guess.includes(letter)) {
+            // Add incorrect letter to guessed letter list
+            guess.push(letter)
+            // Decrement the number of remaining guesses
+            numOfGuesses--
+        } //if numOfGuesses gets to zero reset game
+        if (numOfGuesses === 0) {
         }
     }
 }
-if (!foundLetter) {
-    // Check if incorrect guess is already on the list
-    if (!guess.includes(letter)) {
-        // Add incorrect letter to guessed letter list
-        guess.push(letter)
-        // Decrement the number of remaining guesses
-        numOfGuesses--
-    }
-    if (numOfGuesses === 0) {
-        // Display word before resetting game
-        guess = words.split()
-    }
-}
-}
-
-
-
-    // var remainingLetters = words.length;
-
-    // while (remainingLetters > 0) {
-    //     // Show the player their progress
-    //     console.log(currentWord.join(" "));
-    //     // Take input from the player
-    //     var word = prompt("Guess a letter!");
-    //     console.log(word);
-    //     if (word === null) {
-    //         break;
-    //     } else if (word.length !== 1) {
-    //         console.log("Please enter a single letter.");
-    //     } else {
-    //         // Update answerArray and remainingLetters for every correct guess
-    //         for (var j = 0; j < words.length; j++) {
-    //             if (words[j] === word) {
-    //                 answers[j] = word;
-    //                 remainingLetters--;
-    //             }
-    //         }
-    //     }
-    //     //for loop to add guesses in to answers array
-    //     for (i = 0; i < words.length; i++) {
-    //         words[i]
-    //         for (z = 0; z < guess.length; z++) {
-    //             if (guess[z] === currentWord[i]) {
-    //                 guess.push(currentWord[z])
-    //                 console.log("correct " + currentWord)
-    //             }
-    //             else {
-    //                 guess.push(currentWord[z])
-    //                 console.log("incorrect " + guess)
-    //             }
-    //         };
-    //     }
-    // }
-
 
 directionsText.textContent = " ";
 currentWordText.textContent = "Current word: " + currentWord;
 numOfGuessesText.textContent = "Guesses Remaining: " + numOfGuesses;
 guessText.textContent = "Guess: " + guess;
 winsText.textContent = "wins: " + wins;
+
 
 
 
